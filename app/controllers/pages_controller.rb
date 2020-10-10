ecdb6f58b5254ff79bbe5f6c145af1e629b634e8
bsamm <brandonc.sammons@gmail.com>
2020-10-10 16:50:49 -0500

    display led status better

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   def home
     @devices = Device.all
+    @led_status = WebConnectedLed.status
   end
 
   def toggle_led
     WebConnectedLed.toggle
+    redirect_to root_path
   end
 end
